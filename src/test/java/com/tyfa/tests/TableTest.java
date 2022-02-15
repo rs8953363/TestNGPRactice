2031c9fae6a118ea49e4c4f83d908c7ff4cf5255
Rose <rs8953363@gmail.com>
2022-02-15 02:33:24 -0600

    ...

--- src/test/java/com/tyfa/tests/TableTest.java
+++ src/test/java/com/tyfa/tests/TableTest.java
@@ -9,7 +9,7 @@ import java.util.*;
 
 public class TableTest {
 
-    W3Schools school = new W3Schools();
+    W3Schools schools = new W3Schools();
 
     @Test
     public void allTitles(){
@@ -18,19 +18,25 @@ public class TableTest {
 
         //List<WebElement>
 
-        for (WebElement element: school.titles){
+        for (WebElement element: schools.titles){
             System.out.println(element.getText());
         }
     }
     @Test
     public void allCells(){
+        String url = "https://www.w3schools.com/html/html_tables.asp";
+        Driver.getDriver().get(url);
 
+        for (WebElement element: schools.cells){
+            System.out.println(element.getText());
+        }
     }
 
     @Test
     public void allTitlesReusableMethods(){
         String url = "https://www.w3schools.com/html/html_tables.asp";
         Driver.getDriver().get(url);
+
         List<WebElement> list = ReusableMethods.getTitles();
 
         for (WebElement element : list){
@@ -40,10 +46,10 @@ public class TableTest {
 
     @Test
     public void rowColumnTest(){
-        Test
-        public void allTitlesReusableMethods(){
             String url = "https://www.w3schools.com/html/html_tables.asp";
             Driver.getDriver().get(url);
 
+            WebElement element = ReusableMethods.getRowAndColumn(5,3);
+            System.out.println(element.getText());
     }
 }
--- src/test/java/com/tyfa/utilities/ReusableMethods.java
+++ src/test/java/com/tyfa/utilities/ReusableMethods.java
@@ -12,9 +12,10 @@ public class ReusableMethods {
             e.printStackTrace();
         }
     }
+//We don't use page object model here
 
     public static List<WebElement> getTitles(){
-        return Driver.getDriver().findElements(By.xpath("(//table)["+table+"]//th"));
+        return Driver.getDriver().findElements(By.xpath("(//table)[1]//th"));
     }
 
     public static List<WebElement> getRows(int table){
@@ -22,18 +23,18 @@ public class ReusableMethods {
     }
 
     public static List<WebElement> getAllCells(){
-        return Driver.getDriver().findElements(By.xpath("(//table)[1]/tbody/td"));
+        return Driver.getDriver().findElements(By.xpath("(//table)[1]/tbody//td"));
     }
 
     public static List<WebElement> getCellsInRows(int row){
-        return Driver.getDriver().findElements(By.xpath("((//table)[1]/tbody/tr)["+row+"]"));
+        return Driver.getDriver().findElements(By.xpath("((//table)[1]/tbody/tr)[" +row+ "]"));
     }
 
     public static List<WebElement> getColumns(int column){
-        return Driver.getDriver().findElements(By.xpath("(//table)[1]//tr/td["+column+"]"));
+        return Driver.getDriver().findElements(By.xpath("(//table)[1]//tr/td[" +column+ "]"));
     }
 
-    public static WebElement getRowsAndColumns(int row, int column){
+    public static WebElement getRowAndColumn(int row, int column){
         return Driver.getDriver().findElement(By.xpath("(//table)[1]//tr[" +row+ "]/td[" +column+ "]"));
     }
 }
